681e81d9194979a78d2aa3bac3764efc6342a923
Lareta064 <lareta064@gmail.com>
2025-01-27 11:56:29 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", function (){
-	
+	const bodyEl = document.body;
+	const menuButton = document.querySelector('.menu-button');
+	const mobileMenu = document.querySelector('#mobile-menu');
+	menuButton.addEventListener('click', ()=> {
+		if( menuButton.classList.contains('active')){
+			menuButton.classList.remove('active');
+			mobileMenu.classList.remove('active');
+		}else{
+			menuButton.classList.add('active');
+			mobileMenu.classList.add('active');
+		}
+	});
 
 });
