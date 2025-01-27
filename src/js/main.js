420d2140e34cc40e2ad78af0acd62d5824051889
Lareta064 <lareta064@gmail.com>
2025-01-27 15:26:02 +0300

    fix-mob menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (){
 	const bodyEl = document.body;
+	/*open mobile menu */
 	const menuButton = document.querySelector('.menu-button');
 	const mobileMenu = document.querySelector('#mobile-menu');
 	menuButton.addEventListener('click', ()=> {
@@ -11,5 +12,21 @@ document.addEventListener("DOMContentLoaded", function (){
 			mobileMenu.classList.add('active');
 		}
 	});
+
+	/*open drop menu */
+	const hasDropItems = mobileMenu.querySelectorAll('.has-drop');
+	if(hasDropItems.length>0){
+		hasDropItems.forEach((item)=>{
+			item.addEventListener('click', ()=>{
+				if(item.classList.contains('active')){
+					item.classList.remove('active');
+					item.querySelector('.header-sub-menu').classList.remove('active');
+				}else{
+					item.classList.add('active');
+					item.querySelector('.header-sub-menu').classList.add('active');
+				}
+			});
+		});
+	}
 
 });
