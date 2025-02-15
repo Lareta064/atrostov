d99ecddae667a609e7aa6a86374c43fb94c43038
Lareta064 <lareta1@yandex.ru>
2025-02-15 23:17:57 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -536,5 +536,59 @@ document.addEventListener("DOMContentLoaded", function (){
 		speed:800,
 		items:1,
 		dots: true,
-	})
+	});
+
+	//dropdown
+	const dropdown = document.querySelectorAll('.dropdown');
+	
+	if(dropdown){
+		dropdown.forEach(function (dropDownWrapper) {
+			const dropDownBtn = dropDownWrapper.querySelector('.dropdown__button');
+			const dropDownList = dropDownWrapper.querySelector('.dropdown__list');
+
+			const dropDownListItems = dropDownList.querySelectorAll('.dropdown__list-item');
+			const dropDownInput = dropDownWrapper.querySelector('.dropdown__input-hidden');
+			const dropDownSelected = dropDownList.querySelector('.selected');
+			
+			// Клик по кнопке. Открыть/Закрыть select
+			dropDownBtn.addEventListener('click', function (e) {
+				dropDownList.classList.toggle('dropdown__list--visible');
+				this.classList.toggle('dropdown__button--active');
+			});
+				
+
+			// Выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
+			dropDownListItems.forEach(function (listItem) {
+				listItem.addEventListener('click', function (e) {
+				e.stopPropagation();
+				dropDownListItems.forEach(function (item){item.classList.remove('selected')});
+				
+				dropDownBtn.innerText = this.dataset.value;
+				dropDownBtn.focus();
+				if(dropDownInput){dropDownInput.value = this.dataset.value;}
+				this.classList.add('selected')
+				dropDownList.classList.remove('dropdown__list--visible');
+				dropDownBtn.classList.remove('dropdown__button--active');
+				
+				});
+			});
+
+			// Клик снаружи дропдауна. Закрыть дропдаун
+			document.addEventListener('click', function (e) {
+				if (e.target !== dropDownBtn) {
+				dropDownBtn.classList.remove('dropdown__button--active');
+				dropDownList.classList.remove('dropdown__list--visible');
+				}
+			});
+
+			// Нажатие на Tab или Escape. Закрыть дропдаун
+			document.addEventListener('keydown', function (e) {
+				if (e.key === 'Tab' || e.key === 'Escape') {
+				dropDownBtn.classList.remove('dropdown__button--active');
+				dropDownList.classList.remove('dropdown__list--visible');
+				}
+			});
+		});
+	}
+
 });
